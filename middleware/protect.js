c221ff1312a81ee4594287875ffa4b013256bad2
frankiewilson1 <frankwilliamsugwu@gmail.com>
2026-03-16 06:04:03 -0700

    feat: add protection middleware

---
+++ middleware/protect.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+    let token;
+
+    // Check if Authorization header exists and starts with "Bearer"
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            // Extracts token from "Bearer"
+            token = req.headers.authorization.split(" ")[1];
+
+            // Verify the token and decode the payload
+            const decoded = jwt.verify(token, ProcessingInstruction.env.JWT_SECRET);
+
+            // Attaches the user to the request object (excluding password)
+            req.user = await User.findById(decoded.id).select("-password");
+
+            // passes control to the next function
+            next();
+        } catch (error) {
+            res.status(401).json({ message: "Not authorized, token failed "});
+        }
+    }
+
+    if (!token) {
+        res.status(401).json({ message: "Not authorized, no token "});
+    }
+};
+
+module.exports = protect;
